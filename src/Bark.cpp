cbf59fab0c699c70959d4d4cfc2f9522f8580c31
Phil Golden <12150421+Coirt@users.noreply.github.com>
2019-02-18 13:53:16 +0000

    0.6.4

--- src/Bark.cpp
+++ src/Bark.cpp
@@ -7,9 +7,14 @@ void init(rack::Plugin *p) {
 	plugin = p;
 	p->slug = TOSTRING(SLUG);
 	p->version = TOSTRING(VERSION);
+// 	p->website = "https://github.com/Coirt/Bark/issues";
+// 	p->manual = "https://github.com/Coirt/Bark/wiki";
 
 	// p->addModel(model---);
 	p->addModel(modelTrimLFO);
+	p->addModel(modelTrimLFObpm);
 	p->addModel(modelQuadLogic);
 	p->addModel(modelPanel6);
+	p->addModel(modelOneBand);
+	
 }
