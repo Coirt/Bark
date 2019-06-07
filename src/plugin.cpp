a256ce4878868ef6fe20ae049ad63a5ea2abc1e6
Phil Golden <12150421+Coirt@users.noreply.github.com>
2019-06-07 20:09:46 +0100

    Add PolyMix

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -8,12 +8,12 @@ void init(rack::Plugin *p) {
 
 	// p->addModel(model---);
 	p->addModel(modelTrimLFO);
-	//p->addModel(modelQuadLogic);
-	//p->addModel(modelPanel6);
+	p->addModel(modelQuadLogic);
+	p->addModel(modelPanel6);
 	p->addModel(modelOneBand);
 	p->addModel(modelbpmTrimLFO);
 	p->addModel(modelTestPanel);
 	//p->addModel(modelNavPanel);
 	//p->addModel(modelClockSeq16);
-
+	p->addModel(modelPolyMix);
 }
