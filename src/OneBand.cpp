87a0d438af1054db85510b9999e6f69f7be43934
Phil Golden <12150421+Coirt@users.noreply.github.com>
2019-07-19 03:28:27 +0100

    Bug fix

--- src/OneBand.cpp
+++ src/OneBand.cpp
@@ -76,7 +76,6 @@ struct OneBand : Module {
 		NUM_LIGHTS = dBpeak_LIGHT + 8			//7 + Clipping Light
 	};
 
-	//instantiate
 	dsp::VuMeter2 volUnitIndicatorPEAK;
 	dsp::ClockDivider vuDivider;
 	dsp::ClockDivider lightDivider;
@@ -84,91 +83,101 @@ struct OneBand : Module {
 	Biquad *parametricEQR = new Biquad();
 	LadderFilter lpf24;		//smoothing
 
-	OneBand() { 
+	OneBand() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		//Knob---
 		configParam(EQGAIN_PARAM, -6.5f, 6.5f, 0.f, "Band Gain", "dB", params[EQGAIN_PARAM].getValue(), 5.f);
-		configParam(EQFREQ_PARAM, .459435f, 10.f, 3.459432f, "Freq", " Hz", 2.f, 20.f);
+		configParam(EQFREQ_PARAM, .459435f, 10.f, 3.459432f, "Freq", " Hz", 2, 20);	// .01375f
 		configParam(EQBANDWIDTH_PARAM, .1f, 40.f, 15.f, "Q Factor");
-		configParam(OUTGAIN_PARAM, 0.f, 7.f, 2.f, "Output Gain", "dB", -10, 20, -13.f);
+		configParam(OUTGAIN_PARAM, 0.f, 7.f, 2.f, "Output Gain", "dB", -10, 20, -13.f);	//needs diff offset, TODO: show 0dB by default? or meter dB?
 		//Switch---
 		configParam<tpEQstatus>(EQBYPASS_PARAM, 0.f, 1.f, 0.f, "EQ");
 		configParam<tpGainVal>(PREPOST_PARAM, 0.f, 1.f, 0.f, "Meter", " Gain");
 		configParam<tpSwapLR>(SWAPLR_PARAM, 0.f, 1.f, 0.f, "Output");
 		configParam<tpEQprocess>(LISTEN_PARAM, 0.f, 1.f, 0.f, "EQ");
 
+		//TODO vumeter2
 		volUnitIndicatorPEAK.lambda = 1 / 0.1f;
 		vuDivider.setDivision(16);
 		lightDivider.setDivision(256);
 	}
-	
+
 	void process(const ProcessArgs &args) override {
 		bool Listen = params[LISTEN_PARAM].getValue(), swapLR = params[SWAPLR_PARAM].getValue();
 		float inL = 0.0f, inR = 0.0f, outLdBVU = 0.0f, outRdBVU = 0.0f,
-			outL = simd::clamp(outputs[OUTL_OUTPUT].getVoltage(), -9.9f, 9.9f), outR = simd::clamp(outputs[OUTR_OUTPUT].getVoltage(), -9.9f, 9.9f),
+			outL = clamp(outputs[OUTL_OUTPUT].getVoltage(), -9.9f, 9.9f), outR = clamp(outputs[OUTR_OUTPUT].getVoltage(), -9.9f, 9.9f),
 			Gain = params[OUTGAIN_PARAM].getValue() / 2.0f;
-		double eqGain = params[EQGAIN_PARAM].getValue() * 5.0 + (simd::clamp(inputs[GAINMOD_INPUT].getVoltage(), -6.5f, 6.5f) * 5.0),
-			modInput, eqFreq,
-			eqQ = simd::clamp(params[EQBANDWIDTH_PARAM].getValue(), .1f, 40.f) + (4.f * simd::clamp(inputs[BWMOD_INPUT].getVoltage(), .1f, 40.f));
-		double sampRate = args.sampleRate, biquadFreq = 3.459432, biquadQ = 15., biquadGain = 0.;
+		double eqGain = params[EQGAIN_PARAM].getValue() * 5.0 + (clamp(inputs[GAINMOD_INPUT].getVoltage(), -6.5f, 6.5f) * 5.0), modInput, eqFreq,
+			eqQ = clamp(params[EQBANDWIDTH_PARAM].getValue(), .1f, 40.f) + (4.f * clamp(inputs[BWMOD_INPUT].getVoltage(), .1f, 40.f));
+		double sampRate, biquadFreq, biquadQ, biquadGain = 0.;
 
-		//dBmeter on input---------------------------
-		inL = inputs[INL_INPUT].getVoltage();	
+		//dBmeter on input-------------------
+		inL = inputs[INL_INPUT].getVoltage();
 		inR = inputs[INR_INPUT].getVoltage();
 		//dBmeter on output--------------------------
-		outLdBVU = outputs[OUTL_OUTPUT].getVoltage();	
+		outLdBVU = outputs[OUTL_OUTPUT].getVoltage();
 		outRdBVU = outputs[OUTR_OUTPUT].getVoltage();
+
 		//dB Peak Level Indicator-------------------------------------------------------  
 		if (vuDivider.process()) {
-			if (params[PREPOST_PARAM].getValue() == 0.0f && !inputs[INR_INPUT].isConnected()) {//pre L/mon
+			//pre EQ---
+			if (params[PREPOST_PARAM].value == 0.0f && !inputs[INR_INPUT].active) {//pre
 				volUnitIndicatorPEAK.process(args.sampleTime * vuDivider.getDivision(), inL / 10.f);
-			} else if (params[PREPOST_PARAM].getValue() == 1.0f && !inputs[INR_INPUT].isConnected()) {//post L/mon
+			} else if (params[PREPOST_PARAM].value == 1.0f && !inputs[INR_INPUT].active) {//post
 				volUnitIndicatorPEAK.process(args.sampleTime * vuDivider.getDivision(), outLdBVU / 10.f);
 			}
-			if (params[PREPOST_PARAM].getValue() == 0.0f && inputs[INR_INPUT].isConnected()) {//pre L/R
+			//post EQ---
+			if (params[PREPOST_PARAM].value == 0.0f && inputs[INR_INPUT].active) {
+				//volUnitIndicatorPEAK.setValue(((inL / 10.f) + (inR / 10.f)) / 2.f);
 				volUnitIndicatorPEAK.process(args.sampleTime * vuDivider.getDivision(), (inL / 10.f) + (inR / 10.f) / 2.f);
-			} else if (params[PREPOST_PARAM].getValue() == 1.0f && inputs[INR_INPUT].isConnected()) {//post L/R
+			} else if (params[PREPOST_PARAM].value == 1.0f && inputs[INR_INPUT].active) {
+				//volUnitIndicatorPEAK.setValue(((outLdBVU / 10.f) + (outRdBVU / 10.f)) / 2.f);
 				volUnitIndicatorPEAK.process(args.sampleTime * vuDivider.getDivision(), (outLdBVU / 10.f) + (outRdBVU / 10.f) / 2.f);
 			}
 		}
 		if (lightDivider.process()) {
 			lights[dBpeak_LIGHT + 0].setBrightness(volUnitIndicatorPEAK.getBrightness(0.f, 0.f));
 			for (int light = 1; light < 8; light++) {
-				if (params[PREPOST_PARAM].getValue() == 0.0f && !inputs[INR_INPUT].isConnected()) {//pre
+				//TODO: L/R unbalanced inputs will not engage clip light
+				if (params[PREPOST_PARAM].value == 0.0f && !inputs[INR_INPUT].active) {//pre
+					//volUnitIndicatorPEAK.setValue(inL / 10.f);
 					lights[dBpeak_LIGHT + light].setBrightness(volUnitIndicatorPEAK.getBrightness(-3.f * light, -3.f * (light - 1)));
-				} else if (params[PREPOST_PARAM].getValue() == 1.0f && !inputs[INR_INPUT].isConnected()) {//post
+				} else if (params[PREPOST_PARAM].value == 1.0f && !inputs[INR_INPUT].active) {//post
+					//volUnitIndicatorPEAK.setValue(outLdBVU / 10.f);
 					lights[dBpeak_LIGHT + light].setBrightness(volUnitIndicatorPEAK.getBrightness(-3.f * light, -3.f * (light - 1)));
 				}
-				if (params[PREPOST_PARAM].getValue() == 0.0f && inputs[INR_INPUT].isConnected()) {
+				if (params[PREPOST_PARAM].value == 0.0f && inputs[INR_INPUT].active) {
+					//volUnitIndicatorPEAK.setValue(((inL / 10.f) + (inR / 10.f)) / 2.f);
 					lights[dBpeak_LIGHT + light].setBrightness(volUnitIndicatorPEAK.getBrightness(-3.f * light, -3.f * (light - 1)));
-				} else if (params[PREPOST_PARAM].getValue() == 1.0f && inputs[INR_INPUT].isConnected()) {
+				} else if (params[PREPOST_PARAM].value == 1.0f && inputs[INR_INPUT].active) {
+					//volUnitIndicatorPEAK.setValue(((outLdBVU / 10.f) + (outRdBVU / 10.f)) / 2.f);
 					lights[dBpeak_LIGHT + light].setBrightness(volUnitIndicatorPEAK.getBrightness(-3.f * light, -3.f * (light - 1)));
 				}
 			}
 		}
-		//dB Peak Level Indicator-------------------------------------------------------
-		
 
 		if (inputs[FREQMOD_INPUT].isConnected()) {
 			lights[FreqParamOn].setBrightness(0); lights[FreqParamOff].setBrightness(1);
-			modInput = simd::fmin(simd::clamp((inputs[FREQMOD_INPUT].getVoltage() / 5.f), simd::fabs(.01375f), simd::fabs(10.f)), simd::fmax(simd::fabs(.01375f), simd::fabs(10.f)));
-			lpf24.setCutoff(110.f);
-			lpf24.process(modInput, sampRate);
-			eqFreq = simd::clamp(params[EQFREQ_PARAM].getValue(), .11f, .11f) + ((5.f * lpf24.lowpass) * 2);
+			modInput = std::fminf(clamp((inputs[FREQMOD_INPUT].getVoltage() / 5.f), fabs(.022f), fabs(10.f)), std::fmaxf(fabs(.022f), fabs(10.f)));
+			lpf24.setCutoff(27.5f);
+			lpf24.process(modInput, args.sampleTime);
+			float smooth = 5.f * lpf24.lowpass;
+			eqFreq = clamp(smooth * 2, 1.f, 11.f) - .89f;
 		} else if (!inputs[FREQMOD_INPUT].isConnected()) {
 			lights[FreqParamOn].setBrightness(1); lights[FreqParamOff].setBrightness(0);
-			eqFreq = simd::fmax(simd::pow(2.f, params[EQFREQ_PARAM].getValue()), simd::fmin(simd::fabs(1.1726039399558574), simd::fabs(10.)));//min == .01375
+			eqFreq = std::fmax(std::pow(2.f, params[EQFREQ_PARAM].getValue()), std::fmin(fabs(1.1726039399558574), fabs(10.)));//min == .01375
 		}
-
+		sampRate = args.sampleRate;
 		if (inputs[FREQMOD_INPUT].isConnected()) {
 			biquadFreq = eqFreq * 2000.;	//lin
 		} else if (!inputs[FREQMOD_INPUT].isConnected()) {
-			biquadFreq = eqFreq * 20.;//exp
-		}	
+			biquadFreq = eqFreq * 20.;	//exp
+		}
+
 		//Q
 		biquadQ = eqQ;
 		//Bypass EQ
-		params[EQBYPASS_PARAM].getValue() < 1.f ? biquadGain = eqGain :	biquadGain = 0.;
+		params[EQBYPASS_PARAM].getValue() < 1.f ? biquadGain = eqGain : biquadGain = 0.0;
 		//set Biquad Values
 		parametricEQL->setBiquad(bq_type_peak, biquadFreq / sampRate, biquadQ, biquadGain);
 		parametricEQR->setBiquad(bq_type_peak, biquadFreq / sampRate, biquadQ, biquadGain);
@@ -195,11 +204,12 @@ struct OneBand : Module {
 		outputs[OUTR_OUTPUT].setVoltage(outR);
 	}
 
+
 };
 
 
 struct OneBandWidget : ModuleWidget {
-	
+
 	OneBandWidget(OneBand *module) {
 
 		setModule(module);
@@ -231,8 +241,8 @@ struct OneBandWidget : ModuleWidget {
 		addParam(createParam<BarkSwitchSmallSide>(Vec(21.89f, rackY - 161.23f), module, OneBand::SWAPLR_PARAM));
 		addParam(createParam<BarkSwitchSmall>(Vec(40.4f, rackY - 212.39f), module, OneBand::LISTEN_PARAM));
 		//TODO: Screw Positions
-		addChild(createWidget<BarkScrew1>(Vec(box.size.x - 13, 3)));		//pos2
-		addChild(createWidget<BarkScrew2>(Vec(2, 367.2f)));					//pos3
+		addChild(createWidget<BarkScrew1>(Vec(box.size.x - 13, 3)));			//pos2
+		addChild(createWidget<BarkScrew2>(Vec(2, 367.2f)));						//pos3
 		//Light---
 		addChild(createLight<SmallerLightFA<ParamInLight>>(Vec(floatyMcFloatFace, rackY - 280.05f), module, OneBand::FreqParamOn));
 		addChild(createLight<SmallerLightFA<ParamInLight>>(Vec(floatyMcFloatFace, rackY - 261.72f), module, OneBand::FreqParamOff));
@@ -244,8 +254,6 @@ struct OneBandWidget : ModuleWidget {
 		addChild(createLight<BigLight<greenLight>>(Vec(lightXpos, rackY - 87.45f - 12.8f), module, OneBand::dBpeak_LIGHT + 5));
 		addChild(createLight<BigLight<greenLight>>(Vec(lightXpos, rackY - 75.9f - 12.8f), module, OneBand::dBpeak_LIGHT + 6));
 		addChild(createLight<BigLight<greenLight>>(Vec(lightXpos, rackY - 64.35f - 12.8f), module, OneBand::dBpeak_LIGHT + 7));
-
-		
 	}
 };
 Model *modelOneBand = createModel<OneBand, OneBandWidget>("OneBand");
