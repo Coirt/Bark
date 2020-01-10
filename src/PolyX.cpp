ded25be14f6f92bc4e45e0307d5cd172cd481110
Phil Golden <12150421+Coirt@users.noreply.github.com>
2020-01-10 11:42:25 +0000

    v1.2.0
    Added context menu and update mute/open buttons in /res

--- src/PolyX.cpp
+++ src/PolyX.cpp
@@ -36,8 +36,9 @@ struct PolyX : Module {
 	}
 
 	void onReset() override {
-		channels = -1;
+		channels = -1;//Auto
 	}
+
 	void process(const ProcessArgs &args) override {
 		int lastCh = -1;
 		for (int ch = 0; ch < 16; ch++) {
@@ -45,12 +46,6 @@ struct PolyX : Module {
 			if (inputs[MONO_INPUT + ch].isConnected()) {
 				lastCh = ch;
 				params[MUTEFAKE_PARAM + ch].getValue() == 0.f ? volt = inputs[MONO_INPUT + ch].getVoltage() : volt = 0.f;
-				/***PolyMix
-				might need context menu options!
-				When polyaudio, disconnected channels that are unmuted send 10v that DC offsets that channel
-				When polylevel, no issue
-				When polypan,
-				*/
 			} else if (!inputs[MONO_INPUT + ch].isConnected()) {
 				lastCh = ch;
 				params[MUTEFAKE_PARAM + ch].getValue() == 0.f ? volt = 10.f : volt = 0.f;
@@ -69,7 +64,6 @@ struct PolyX : Module {
 					bool isGreen = (c < outputs[POLY_OUTPUT].getChannels() && params[MUTEFAKE_PARAM + c].getValue() == 0.f);
 					lights[chMuteSTATE_LIGHT + c].setBrightness(isRed);
 					lights[chOpenSTATE_LIGHT + c].setBrightness(isGreen);
-
 				}
 				if (inputs[MONO_INPUT + c].isConnected()) {
 					lights[chOpenSTATE_LIGHT + c].setBrightness(0);
@@ -82,6 +76,47 @@ struct PolyX : Module {
 		}
 
 	}//process
+
+	///store channels to json and retrieve when loading patch
+	json_t* dataToJson() override {
+		json_t* rootJ = json_object();
+		json_object_set_new(rootJ, "channels", json_integer(channels));
+		return rootJ;
+	}
+
+	void dataFromJson(json_t* rootJ) override {
+		json_t* channelsJ = json_object_get(rootJ, "channels");
+		if (channelsJ)
+			channels = json_integer_value(channelsJ);
+	}
+};
+
+struct PolyXChannelItem : MenuItem {
+	PolyX* module;
+	int channels;
+	void onAction(const event::Action& e) override {
+		module->channels = channels;
+	}
+};
+
+
+struct PolyXChannelsItem : MenuItem {
+	PolyX* module;
+	Menu* createChildMenu() override {
+		Menu* menu = new Menu;
+		for (int channels = -1; channels <= 16; channels++) {
+			PolyXChannelItem* item = new PolyXChannelItem;
+			if (channels < 0)
+				item->text = "Automatic";
+			else
+				item->text = string::f("%d", channels);
+			item->rightText = CHECKMARK(module->channels == channels);
+			item->module = module;
+			item->channels = channels;
+			menu->addChild(item);
+		}
+		return menu;
+	}
 };
 
 struct PolyXWidget : ModuleWidget {
@@ -89,12 +124,11 @@ struct PolyXWidget : ModuleWidget {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/BarkPolyX.svg")));
 
-		//constexpr int rackY = 380;
 		constexpr float portY[8] = { 303.53f, 268.66f, 233.81f, 198.96f, 164.1f, 129.24f, 94.38f, 59.52f },
-						btnY[8] = { 314.03f, 279.16f, 244.31f, 209.46f, 142.83f, 107.98f, 73.11f, 38.26f };
+				btnY[8] = { 314.03f, 279.16f, 244.31f, 209.46f, 142.83f, 107.98f, 73.11f, 38.26f };
 		constexpr float lightCol1 = 44.12f, lightCol2 = lightCol1 + 6.f, lightCol3 = lightCol2 + 6.f, lightCol4 = lightCol3 + 6.f,
-						lightRow1 = 353.17f, lightRow2 = lightRow1 - 6.1f, lightRow3 = lightRow2 - 6.1f, lightRow4 = lightRow3 - 6.1f,
-						portC1X = 11.78f, portC2X = 39.f, btnC1 = 5.92f, btnC2 = 58.59f;
+				lightRow1 = 353.17f, lightRow2 = lightRow1 - 6.1f, lightRow3 = lightRow2 - 6.1f, lightRow4 = lightRow3 - 6.1f,
+				portC1X = 11.78f, portC2X = 39.f, btnC1 = 5.92f, btnC2 = 58.59f;
 
 		///Ports---
 		//Out---
@@ -117,79 +151,90 @@ struct PolyXWidget : ModuleWidget {
 		addInput(createInput<BarkPatchPortIn>(Vec(portC1X, rackY - portY[7]), module, PolyX::MONO_INPUT + 14));
 		addInput(createInput<BarkPatchPortIn>(Vec(portC2X, rackY - portY[7]), module, PolyX::MONO_INPUT + 15));
 		//Button---
-		addParam(createParam<BarkPushButton2>(Vec(btnC1, rackY - btnY[0]), module, PolyX::MUTEFAKE_PARAM + 0));
-		addParam(createParam<BarkPushButton2>(Vec(btnC2, rackY - btnY[0]), module, PolyX::MUTEFAKE_PARAM + 1));
-		addParam(createParam<BarkPushButton2>(Vec(btnC1, rackY - btnY[1]), module, PolyX::MUTEFAKE_PARAM + 2));
-		addParam(createParam<BarkPushButton2>(Vec(btnC2, rackY - btnY[1]), module, PolyX::MUTEFAKE_PARAM + 3));
-		addParam(createParam<BarkPushButton2>(Vec(btnC1, rackY - btnY[2]), module, PolyX::MUTEFAKE_PARAM + 4));
-		addParam(createParam<BarkPushButton2>(Vec(btnC2, rackY - btnY[2]), module, PolyX::MUTEFAKE_PARAM + 5));
-		addParam(createParam<BarkPushButton2>(Vec(btnC1, rackY - btnY[3]), module, PolyX::MUTEFAKE_PARAM + 6));
-		addParam(createParam<BarkPushButton2>(Vec(btnC2, rackY - btnY[3]), module, PolyX::MUTEFAKE_PARAM + 7));
-		addParam(createParam<BarkPushButton2>(Vec(btnC1, rackY - btnY[4]), module, PolyX::MUTEFAKE_PARAM + 8));
-		addParam(createParam<BarkPushButton2>(Vec(btnC2, rackY - btnY[4]), module, PolyX::MUTEFAKE_PARAM + 9));
-		addParam(createParam<BarkPushButton2>(Vec(btnC1, rackY - btnY[5]), module, PolyX::MUTEFAKE_PARAM + 10));
-		addParam(createParam<BarkPushButton2>(Vec(btnC2, rackY - btnY[5]), module, PolyX::MUTEFAKE_PARAM + 11));
-		addParam(createParam<BarkPushButton2>(Vec(btnC1, rackY - btnY[6]), module, PolyX::MUTEFAKE_PARAM + 12));
-		addParam(createParam<BarkPushButton2>(Vec(btnC2, rackY - btnY[6]), module, PolyX::MUTEFAKE_PARAM + 13));
-		addParam(createParam<BarkPushButton2>(Vec(btnC1, rackY - btnY[7]), module, PolyX::MUTEFAKE_PARAM + 14));
-		addParam(createParam<BarkPushButton2>(Vec(btnC2, rackY - btnY[7]), module, PolyX::MUTEFAKE_PARAM + 15));
+		addParam(createParam<BarkPushButton3>(Vec(btnC1, rackY - btnY[0]), module, PolyX::MUTEFAKE_PARAM + 0));
+		addParam(createParam<BarkPushButton3>(Vec(btnC2, rackY - btnY[0]), module, PolyX::MUTEFAKE_PARAM + 1));
+		addParam(createParam<BarkPushButton3>(Vec(btnC1, rackY - btnY[1]), module, PolyX::MUTEFAKE_PARAM + 2));
+		addParam(createParam<BarkPushButton3>(Vec(btnC2, rackY - btnY[1]), module, PolyX::MUTEFAKE_PARAM + 3));
+		addParam(createParam<BarkPushButton3>(Vec(btnC1, rackY - btnY[2]), module, PolyX::MUTEFAKE_PARAM + 4));
+		addParam(createParam<BarkPushButton3>(Vec(btnC2, rackY - btnY[2]), module, PolyX::MUTEFAKE_PARAM + 5));
+		addParam(createParam<BarkPushButton3>(Vec(btnC1, rackY - btnY[3]), module, PolyX::MUTEFAKE_PARAM + 6));
+		addParam(createParam<BarkPushButton3>(Vec(btnC2, rackY - btnY[3]), module, PolyX::MUTEFAKE_PARAM + 7));
+		addParam(createParam<BarkPushButton3>(Vec(btnC1, rackY - btnY[4]), module, PolyX::MUTEFAKE_PARAM + 8));
+		addParam(createParam<BarkPushButton3>(Vec(btnC2, rackY - btnY[4]), module, PolyX::MUTEFAKE_PARAM + 9));
+		addParam(createParam<BarkPushButton3>(Vec(btnC1, rackY - btnY[5]), module, PolyX::MUTEFAKE_PARAM + 10));
+		addParam(createParam<BarkPushButton3>(Vec(btnC2, rackY - btnY[5]), module, PolyX::MUTEFAKE_PARAM + 11));
+		addParam(createParam<BarkPushButton3>(Vec(btnC1, rackY - btnY[6]), module, PolyX::MUTEFAKE_PARAM + 12));
+		addParam(createParam<BarkPushButton3>(Vec(btnC2, rackY - btnY[6]), module, PolyX::MUTEFAKE_PARAM + 13));
+		addParam(createParam<BarkPushButton3>(Vec(btnC1, rackY - btnY[7]), module, PolyX::MUTEFAKE_PARAM + 14));
+		addParam(createParam<BarkPushButton3>(Vec(btnC2, rackY - btnY[7]), module, PolyX::MUTEFAKE_PARAM + 15));
 		//screw---
-		addChild(createWidget<BarkScrew3>(Vec(2, 3)));							//pos1
-		addChild(createWidget<BarkScrew4>(Vec(box.size.x - 13, 367.2)));			//pos4
+		addChild(createWidget<BarkScrew3>(Vec(2, 3)));					//pos1
+		addChild(createWidget<BarkScrew4>(Vec(box.size.x - 13, 367.2)));		//pos4
 		//Light---
 		///lightRow1
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol1, rackY - lightRow1), module, PolyX::chPolySTATE_LIGHT + 0));//on mono in
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol1, rackY - lightRow1), module, PolyX::chPolySTATE_LIGHT + 0));//on mono in
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol1, rackY - lightRow1), module, PolyX::chMuteSTATE_LIGHT + 0));//on mono in / no connection 
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol1, rackY - lightRow1), module, PolyX::chOpenSTATE_LIGHT + 0));//no connection
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol2, rackY - lightRow1), module, PolyX::chPolySTATE_LIGHT + 1));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol2, rackY - lightRow1), module, PolyX::chPolySTATE_LIGHT + 1));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol2, rackY - lightRow1), module, PolyX::chMuteSTATE_LIGHT + 1));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol2, rackY - lightRow1), module, PolyX::chOpenSTATE_LIGHT + 1));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol3, rackY - lightRow1), module, PolyX::chPolySTATE_LIGHT + 2));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol3, rackY - lightRow1), module, PolyX::chPolySTATE_LIGHT + 2));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol3, rackY - lightRow1), module, PolyX::chMuteSTATE_LIGHT + 2));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol3, rackY - lightRow1), module, PolyX::chOpenSTATE_LIGHT + 2));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol4, rackY - lightRow1), module, PolyX::chPolySTATE_LIGHT + 3));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol4, rackY - lightRow1), module, PolyX::chPolySTATE_LIGHT + 3));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol4, rackY - lightRow1), module, PolyX::chMuteSTATE_LIGHT + 3));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol4, rackY - lightRow1), module, PolyX::chOpenSTATE_LIGHT + 3));
 		///lightRow2
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol1, rackY - lightRow2), module, PolyX::chPolySTATE_LIGHT + 4));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol1, rackY - lightRow2), module, PolyX::chPolySTATE_LIGHT + 4));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol1, rackY - lightRow2), module, PolyX::chMuteSTATE_LIGHT + 4));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol1, rackY - lightRow2), module, PolyX::chOpenSTATE_LIGHT + 4));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol2, rackY - lightRow2), module, PolyX::chPolySTATE_LIGHT + 5));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol2, rackY - lightRow2), module, PolyX::chPolySTATE_LIGHT + 5));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol2, rackY - lightRow2), module, PolyX::chMuteSTATE_LIGHT + 5));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol2, rackY - lightRow2), module, PolyX::chOpenSTATE_LIGHT + 5));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol3, rackY - lightRow2), module, PolyX::chPolySTATE_LIGHT + 6));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol3, rackY - lightRow2), module, PolyX::chPolySTATE_LIGHT + 6));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol3, rackY - lightRow2), module, PolyX::chMuteSTATE_LIGHT + 6));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol3, rackY - lightRow2), module, PolyX::chOpenSTATE_LIGHT + 6));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol4, rackY - lightRow2), module, PolyX::chPolySTATE_LIGHT + 7));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol4, rackY - lightRow2), module, PolyX::chPolySTATE_LIGHT + 7));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol4, rackY - lightRow2), module, PolyX::chMuteSTATE_LIGHT + 7));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol4, rackY - lightRow2), module, PolyX::chOpenSTATE_LIGHT + 7));
 		///lightRow3
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol1, rackY - lightRow3), module, PolyX::chPolySTATE_LIGHT + 8));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol1, rackY - lightRow3), module, PolyX::chPolySTATE_LIGHT + 8));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol1, rackY - lightRow3), module, PolyX::chMuteSTATE_LIGHT + 8));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol1, rackY - lightRow3), module, PolyX::chOpenSTATE_LIGHT + 8));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol2, rackY - lightRow3), module, PolyX::chPolySTATE_LIGHT + 9));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol2, rackY - lightRow3), module, PolyX::chPolySTATE_LIGHT + 9));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol2, rackY - lightRow3), module, PolyX::chMuteSTATE_LIGHT + 9));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol2, rackY - lightRow3), module, PolyX::chOpenSTATE_LIGHT + 9));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol3, rackY - lightRow3), module, PolyX::chPolySTATE_LIGHT + 10));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol3, rackY - lightRow3), module, PolyX::chPolySTATE_LIGHT + 10));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol3, rackY - lightRow3), module, PolyX::chMuteSTATE_LIGHT + 10));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol3, rackY - lightRow3), module, PolyX::chOpenSTATE_LIGHT + 10));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol4, rackY - lightRow3), module, PolyX::chPolySTATE_LIGHT + 11));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol4, rackY - lightRow3), module, PolyX::chPolySTATE_LIGHT + 11));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol4, rackY - lightRow3), module, PolyX::chMuteSTATE_LIGHT + 11));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol4, rackY - lightRow3), module, PolyX::chOpenSTATE_LIGHT + 11));
 		///lightRow4
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol1, rackY - lightRow4), module, PolyX::chPolySTATE_LIGHT + 12));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol1, rackY - lightRow4), module, PolyX::chPolySTATE_LIGHT + 12));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol1, rackY - lightRow4), module, PolyX::chMuteSTATE_LIGHT + 12));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol1, rackY - lightRow4), module, PolyX::chOpenSTATE_LIGHT + 12));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol2, rackY - lightRow4), module, PolyX::chPolySTATE_LIGHT + 13));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol2, rackY - lightRow4), module, PolyX::chPolySTATE_LIGHT + 13));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol2, rackY - lightRow4), module, PolyX::chMuteSTATE_LIGHT + 13));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol2, rackY - lightRow4), module, PolyX::chOpenSTATE_LIGHT + 13));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol3, rackY - lightRow4), module, PolyX::chPolySTATE_LIGHT + 14));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol3, rackY - lightRow4), module, PolyX::chPolySTATE_LIGHT + 14));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol3, rackY - lightRow4), module, PolyX::chMuteSTATE_LIGHT + 14));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol3, rackY - lightRow4), module, PolyX::chOpenSTATE_LIGHT + 14));
-		addChild(createLight<SmallestLight<PolyLight>>(Vec(lightCol4, rackY - lightRow4), module, PolyX::chPolySTATE_LIGHT + 15));
+		addChild(createLight<SmallestLightInverse<PolyLight>>(Vec(lightCol4, rackY - lightRow4), module, PolyX::chPolySTATE_LIGHT + 15));
 		addChild(createLight<SmallestLight<PolyMute>>(Vec(lightCol4, rackY - lightRow4), module, PolyX::chMuteSTATE_LIGHT + 15));
 		addChild(createLight<SmallestLight<PolyFake>>(Vec(lightCol4, rackY - lightRow4), module, PolyX::chOpenSTATE_LIGHT + 15));
+	}
+	
+	void appendContextMenu(Menu* menu) override {
+		PolyX* module = dynamic_cast<PolyX*>(this->module);
 
+		menu->addChild(new MenuEntry);
+
+		PolyXChannelsItem* channelsItem = new PolyXChannelsItem;
+		channelsItem->text = "Channels";
+		channelsItem->rightText = RIGHT_ARROW;
+		channelsItem->module = module;
+		menu->addChild(channelsItem);
 	}
 
 };
