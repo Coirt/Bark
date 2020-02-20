c803b2bece19c7ca94e29654fa1b6087bc0b6854
Phil Golden <12150421+Coirt@users.noreply.github.com>
2020-02-20 01:09:25 +0000

    v1.2.2

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,24 +1,24 @@
-#include "plugin.hpp"
+#include "rack.hpp"
 
-Plugin *pluginInstance;
 
-void init(rack::Plugin *p) {
+using namespace rack;
 
-	pluginInstance = p;
+extern Plugin *pluginInstance;
 
-	// p->addModel(model---);
-	p->addModel(modelTrimLFO);
-	p->addModel(modelQuadLogic);
-	p->addModel(modelPanel6);
-	p->addModel(modelOneBand);
-	p->addModel(modelbpmTrimLFO);
-	p->addModel(modelPolyMix);
-	p->addModel(modelClamp);
-	p->addModel(modelPolyX);
-	p->addModel(modelEOsum);
-	p->addModel(modelSHTH);
-	p->addModel(modelSHTH2);
-	p->addModel(modelLMH);
-	p->addModel(modelLMH2);
-}
+//Modules
+extern Model *modelTrimLFO;
+extern Model *modelQuadLogic;
+extern Model *modelPanel6;
+extern Model *modelOneBand;
+extern Model *modelbpmTrimLFO;
+extern Model *modelPolyMix;
+extern Model *modelClamp;
+extern Model *modelPolyX;
+extern Model *modelEOsum;
+//extern Model *modelSHTH;
+//extern Model *modelSHTH2;
+extern Model *modelSHTH;
+extern Model *modelLMH;
+//extern Model *modelLMH2;
+extern Model *modeltestButtonDelay;
 
